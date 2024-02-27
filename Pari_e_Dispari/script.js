c23f8081f1a9cc369623a60d0ab4af65fbe7c542
danielecolaci <danielecolaci@outlook.com>
2024-02-27 15:44:08 +0100

    Add function and condition pari o dispari

--- Pari_e_Dispari/script.js
+++ Pari_e_Dispari/script.js
@@ -3,10 +3,25 @@ console.log('ok');
 let choiseInput = prompt('Pari o Dispari?')
 let numberInput = parseInt(prompt('Inserisci un numero da 1 a 5'));
 
-let choiseComputer = numberCasual();
+let numberComputer = numberCasual();
 
 function numberCasual() {
     return Math.floor(Math.random() * 5) + 1;
 }
 
-console.log(choiseInput, numberInput, choiseComputer);
+console.log(choiseInput, numberInput, numberComputer);
+
+
+let somma = numberInput + numberComputer
+
+console.log(somma);
+
+function numberPari(numb) {
+    return numb % 2 === 0;
+}
+
+if (numberPari(somma)) {
+    console.log('è pari');
+} else {
+    console.log('è dispari');
+}
