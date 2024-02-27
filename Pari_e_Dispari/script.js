738a9445986b26bd22e3057cb1ee7c45680f3471
danielecolaci <danielecolaci@outlook.com>
2024-02-27 16:02:50 +0100

    Add Result

--- Pari_e_Dispari/script.js
+++ Pari_e_Dispari/script.js
@@ -1,6 +1,6 @@
 console.log('ok');
 
-let choiseInput = prompt('Pari o Dispari?')
+let choiseInput = prompt('Pari o Dispari?');
 let numberInput = parseInt(prompt('Inserisci un numero da 1 a 5'));
 
 let numberComputer = numberCasual();
@@ -20,8 +20,12 @@ function numberPari(numb) {
     return numb % 2 === 0;
 }
 
-if (numberPari(somma)) {
-    console.log('è pari');
+let result = numberPari(somma) ? 'pari' : 'dispari';
+
+console.log(result);
+
+if (result === choiseInput.toLowerCase()) {
+    alert('Hai vinto');
 } else {
-    console.log('è dispari');
+    alert('Hai perso');
 }
